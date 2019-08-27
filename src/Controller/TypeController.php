cb021d255ba79f7f0207818a9a57849456805fd6
Tom Sihap <sihapanya.thomas@gmail.com>
2019-08-27 12:35:05 +0200

    Delete button

--- src/Controller/TypeController.php
+++ src/Controller/TypeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Type;
 use App\Form\TypeType;
+use App\Repository\TypeRepository;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -32,9 +33,13 @@ class TypeController extends AbstractController
     /**
      * @Route("/new", name="type_create", methods={"POST", "GET"})
      * @param Request $request
+     * @param ObjectManager $entityManager
+     * @param TypeRepository $typeRepository
      * @return Response
      */
-    public function create(Request $request)
+    public function create(Request $request,
+                           ObjectManager $entityManager,
+                           TypeRepository $typeRepository)
     {
 
         $type = new Type();
@@ -45,15 +50,34 @@ class TypeController extends AbstractController
 
         if ( $form->isSubmitted() && $form->isValid() )
         {
-
-            $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($type);
-
+            $entityManager->flush();
         }
 
         return $this->render('type/create.html.twig', [
-            'form' => $form->createView()
+            'form'  => $form->createView(),
+            'types' => $typeRepository->findAll()
         ]);
 
+    }
+
+    /**
+     * @Route("/{id}/delete", name="type_delete", methods={"DELETE"})
+     * @param Type $type
+     */
+    public function delete(Request $request, Type $type)
+    {
+        $token = $request->request->get('_token');
+
+        if ( $this->isCsrfTokenValid('deleteType' . $type->getId(), $token) )
+        {
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($type);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('type_create');
+
+        }
     }
 }
