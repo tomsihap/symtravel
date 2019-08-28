a6a55f7a8520b2cfe116b9d4de3b58f489ccb9db
Tom Sihap <sihapanya.thomas@gmail.com>
2019-08-28 11:49:30 +0200

    Upload files

---
+++ src/Controller/DestinationController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Destination;
+use App\Form\DestinationType;
+use App\Repository\DestinationRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class DestinationController
+ * @package App\Controller
+ *
+ * @Route("/destinations")
+ */
+class DestinationController extends AbstractController
+{
+    /**
+     * @Route("/", name="destination_index")
+     */
+    public function index(DestinationRepository $destinationRepository)
+    {
+        return $this->render('destination/index.html.twig', [
+            'destinations' => $destinationRepository->findAll()
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="destination_create")
+     * @Route("/{id}/edit", name="destination_edit")
+     */
+    public function create(Request $request, Destination $destination = null)
+    {
+
+        $isEditMode = true;
+
+        if ($destination === null)
+        {
+            $destination = new Destination();
+            $isEditMode = false;
+        }
+
+        $form = $this->createForm(DestinationType::class, $destination);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            // Traitement de la photo
+
+            $photo = $form['photo']->getData();
+
+            if ($photo) {
+
+                $originalFilename = pathinfo($photo->getClientOriginalName(), PATHINFO_FILENAME);
+
+                $safeFilename = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
+
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$photo->guessExtension();
+
+                try {
+                    $photo->move(
+                        $this->getParameter('destinations_photos_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    dd($e);
+                }
+
+                $destination->setPhoto($newFilename);
+            }
+
+
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($destination);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('destination_index');
+        }
+
+        return $this->render('destination/create.html.twig', [
+            'form' => $form->createView(),
+            'isEditMode' => $isEditMode
+        ]);
+    }
+}
---
+++ src/Form/DestinationType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Destination;
+use App\Entity\Type;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
+class DestinationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('country')
+            ->add('photo', FileType::class, [
+                'label'     => 'Photo de la destination',
+                'mapped'    => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '8M',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/gif',
+                        ],
+                        'mimeTypesMessage' => 'Merci d\'uploader une image valide.',
+                    ])
+                ],
+
+            ])
+            ->add('type', EntityType::class, [
+                'choice_label' => 'name',
+                'class' => Type::class
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Destination::class,
+        ]);
+    }
+}
