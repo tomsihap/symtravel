439a4ca104d2a39f5f4e62e41cc118e42b7011de
Tom Sihap <sihapanya.thomas@gmail.com>
2019-08-29 16:47:02 +0200

    ajout commentaire

--- src/Form/ReviewType.php
+++ src/Form/ReviewType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Review;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
